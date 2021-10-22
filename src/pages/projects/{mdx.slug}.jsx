c608d841aa5cb1873925dc5ebd727686b9916ad5
roofWalker <roofwalker13@outlook.com>
2021-10-22 11:57:34 -0700

    site build and plugin errors resolved

--- src/pages/projects/{mdx.slug}.jsx
+++ src/pages/projects/{mdx.slug}.jsx
@@ -2,18 +2,30 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-import { Box, Container } from "@material-ui/core";
+import { Box, Container, Grid, Paper } from "@material-ui/core";
 
 import Layout from "../../components/LayoutComponent";
 
 const FeaturedProject = ({ data }) => {
     return (
         <Layout>
-            <h1>Content will go here</h1>
+            <Paper>
+
+                <Box>
+                    <Container>
+                        <Grid container>
+                            <Grid item>
+
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </Paper>
             <Box px={{ xs: 3, sm: 5 }} py={{ xs: 15, sm: 15 }} >
                 <Container maxWidth="lg">
-                    <Link> Back </Link>
-                    <h1>Featured Project Content</h1>
+                    <h1>{data.mdx.frontmatter.title}</h1>
+                    <h3>{data.mdx.frontmatter.short_description}</h3>
+                    <h3>{data.mdx.frontmatter.full_description}</h3>
                 </Container>
             </Box>
         </Layout>
