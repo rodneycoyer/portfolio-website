c322cd510dc82e2033763e91842f9d65ced7fd07
rodneycoyer <rodney.coyer@gmail.com>
2022-03-24 13:28:36 -0700

    netlify cli update

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
         path: `${__dirname}/projects`,
       },
     },
-    { // mdx images
+    { // process for mdx use
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects-images`,
